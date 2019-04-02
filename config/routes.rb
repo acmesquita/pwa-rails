adc74298df3c27995fbcbd7960e76ff1ccb2475f
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2019-04-02 09:57:53 -0300

    Create pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/blog'
+  get 'pages/courses'
+  get 'pages/bootcamp'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'page#home'
 end
