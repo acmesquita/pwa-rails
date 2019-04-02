17fcb91d509c4cde6f31b654ab283a5a8dac0b3f
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2019-04-02 10:23:17 -0300

    Fixing router

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'pages/courses'
   get 'pages/bootcamp'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'page#home'
+  root to: 'pages#home'
 end
